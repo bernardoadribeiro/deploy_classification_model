e1563206cf14ff053ea8d2c66efb7640ce07614b
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-02-09 22:54:41 -0300

    feat: create iris predict endpoint

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import pickle
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
-
 ## Import prediction models
-#iris_model = pickle.load(open('models/modelo.pkl', 'rb'))
+iris_model = pickle.load(open('models/iris_decision_tree_model.pkl', 'rb'))
+
+
+app = Flask(__name__)
 
 
 ## App routes ##
@@ -14,10 +14,18 @@ def index():
     """Index route. Load index page."""
     return render_template('index.html')
 
-@app.route('/predict/iris/')
-def predict():
-    """Returns the predictions based on the input data."""
-    return render_template()
+@app.route('/predict/iris/<float:sw>/<float:sl>/<float:pw>/<float:pl>', methods=['GET'])
+def predict(sw, sl, pw, pl):
+    """
+        Returns the predictions based on the input data.
+
+        sw: SepalWidth
+        sl: SepalLeight
+        pw: PetalWidth
+        pl: PetalLeight
+    """
+    result = iris_model.predict([[sw, sh, pw, ph]])
+    return "Class: {}".format(result)
 
 ## App config ##
 app.debug = True
@@ -25,4 +33,4 @@ app.debug = True
 
 ## App start ##
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, host='0.0.0.0')
