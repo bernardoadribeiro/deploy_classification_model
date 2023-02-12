0bf7d52ab0a5fadb1c6f4b0f99248f1d69d019ae
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-02-12 14:36:10 -0300

    fix: fix env.sample and app port

--- main.py
+++ main.py
@@ -117,4 +117,4 @@ def product_category():
 
 ## App start ##
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True, host='0.0.0.0')
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', port=5000)
